0799a1d3ad52d28b08e75f0c0509006c1654195a
Mark Jantz <mjantz@crcfe01.crc.nd.edu>
2023-10-11 23:07:38 -0400

    mark4

--- ref_sequences/masterscript.sh
+++ ref_sequences/masterscript.sh
@@ -1,10 +1,21 @@
 
 for f in hsp70gene_*.fasta; do
-cat "$f"|tail -n +2 >> "hsp70combined.fasta";
+cat "$f" >> "hsp70combined.fasta";
 
 done
 
 for f in mcrAgene_*.fasta; do
-cat "$f"|tail -n +2 >> "mcrAgenecombined.fasta";
+cat "$f" >> "mcrAgenecombined.fasta";
+
+done
+
+
+~/Private/Biocomputing/tools/muscle -align hsp70combined.fasta -output hsp70aligned.fasta
+~/Private/Biocomputing/tools/hmmbuild hsp70.hmm hsp70aligned.fasta
+
+for f in {1:50}; do
+
+~/Private/Biocomputing/BioinformaticsProject/proteomes/proteome_01.fasta
+~/Private/Biocomputing/tools/hmmsearch -tblout hsp70.output hsp70.hmm
 
 done
