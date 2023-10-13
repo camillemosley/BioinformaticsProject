b69af1fd3b9d9d811f533b6ae262e31d71b4a264
Rhett Bianchi <ebianch2@crcfe01.crc.nd.edu>
2023-10-13 17:43:16 -0400

    BIGprogress

--- projectscript.sh
+++ projectscript.sh
@@ -1,51 +1,48 @@
 #!/bin/bash
 
-# Combine all of the reference sequences for hsp70 into a single file called hsp70_combined.fasta
+# Define paths
+MUSCLE_PATH=~/Private/Biocomputing/tools/muscle3.8.31_i86linux64
+HMMBUILD_PATH=~/Private/Biocomputing/tools/hmmbuild
+HMMSEARCH_PATH=~/Private/Biocomputing/tools/hmmsearch
+REF_SEQ_PATH=~/Private/Biocomputing/BioinformaticsProject/ref_sequences
+PROTEOMES_PATH=~/Private/Biocomputing/BioinformaticsProject/proteomes
 
-cd ~/Biocomputing/BioinformaticsProject/ref_sequences
+# Combine all of the reference sequences for hsp70 and mcrA
+cd $REF_SEQ_PATH
 cat hsp70gene*.fasta > hsp70_combined.fasta
-
-# Combine all of the reference sequences for mcrA into a single file called mcrA_combined.fasta
-
 cat mcrAgene*.fasta > mcrA_combined.fasta
 
-# Sequence alignment with muscle for hsp70_combined.fasta
+# Sequence alignment with muscle
+$MUSCLE_PATH -in hsp70_combined.fasta -out hsp70_aligned.fasta
+$MUSCLE_PATH -in mcrA_combined.fasta -out mcrA_aligned.fasta
 
-~/Biocomputing/tools/muscle3.8.31_i86linux64 -in hsp70_combined.fasta -out hsp70_aligned.fasta
+# hmmbuild
+$HMMBUILD_PATH hsp70.hmm hsp70_aligned.fasta
+$HMMBUILD_PATH mcrA.hmm mcrA_aligned.fasta
 
-# Sequence alignment with muscle for mcrA_combined.fasta
-
-~/Biocomputing/tools/muscle3.8.31_i86linux64 -in mcrA_combined.fasta -out mcrA_aligned.fasta
-
-# hmmbuild for hsp70_aligned.fasta
-
-~/Biocomputing/tools/hmmbuild hsp70.hmm hsp70_aligned.fasta
-
-# hmmbuild for mcrA_aligned.fasta
-
-~/Biocomputing/tools/hmmbuild mcrA.hmm mcrA_aligned.fasta
+# Move HMM files to PROTEOMES_PATH for easier access
+mv hsp70.hmm $PROTEOMES_PATH
+mv mcrA.hmm $PROTEOMES_PATH
 
 # Create a CSV file to store the results
-
-echo "Proteome, HSP70 Matches, McrA Matches" > BioinformaticsProject/proteomes/proteome_analysis_results.csv
+echo "Proteome, HSP70 Matches, McrA Matches" > $PROTEOMES_PATH/proteome_analysis_results.csv
 
 # hmmsearch for each of the proteomes
-
-cd ~/Biocomputing/BioinformaticsProject/proteomes
+cd $PROTEOMES_PATH
 for proteome in proteome_*.fasta
 do
     # Search for hsp70 matches
-    ~/Biocomputing/tools/hmmsearch --tblout hsp70_results.txt hsp70.hmm $proteome
+    $HMMSEARCH_PATH --tblout hsp70_results.txt hsp70.hmm $proteome
     hsp70_matches=$(grep -v "#" hsp70_results.txt | wc -l)
 
     # Search for mcrA matches
-    ~/Biocomputing/tools/hmmsearch --tblout mcrA_results.txt mcrA.hmm $proteome
+    $HMMSEARCH_PATH --tblout mcrA_results.txt mcrA.hmm $proteome
     mcrA_matches=$(grep -v "#" mcrA_results.txt | wc -l)
 
     # Append results to the CSV file
-    echo "$proteome, $hsp70_matches, $mcrA_matches" >> BioinformaticsProject/proteomes/proteome_analysis_results.csv
+    echo "$proteome, $hsp70_matches, $mcrA_matches" >> proteome_analysis_results.csv
 done
 
 # Clean up intermediate files
-
 rm hsp70_results.txt mcrA_results.txt
+
