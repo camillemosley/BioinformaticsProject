9547a1c9e556fa8db0db33de5ec9ef59021c998e
Max Zarodniuk <plezar@maxs-mbp-3.dhcp.nd.edu>
2023-10-13 13:50:56 -0400

    WIP

--- code.sh
+++ code.sh
@@ -1,11 +1,16 @@
-# path to working directory (BioinformaticsProject)
+###		Example usage: bash script.sh /afs/crc.nd.edu/user/m/mzarodn2/Private/Biocomputing/BioinformaticsProject ref_sequences proteomes
 
-# WD_PATH=/afs/crc.nd.edu/user/m/mzarodn2/Private/Biocomputing/BioinformaticsProject
+### 	Arguments
+#		WD_PATH – path to working directory
+#		REFSEQ_DIRNAME – directory containing reference sequences; must be a subdirectory in WD_PATH
+#		PROTEOME_DIRNAME – directory containing proteome sequences; must be a subdirectory in WD_PATH
+
 WD_PATH=$1
+REFSEQ_DIRNAME=$2
+PROTEOME_DIRNAME=$3
 
 # Concatenating all reference sequences into one fasta file
-cd ${WD_PATH}/ref_sequences
-
+cd ${WD_PATH}/${REFSEQ_DIRNAME}
 cat mcrAgene_*.fasta > mcrAgene.fasta
 cat hsp70gene_*.fasta > hsp70gene.fasta
 
@@ -22,32 +27,23 @@ cd ../
 
 # Hmmbuild
 mkdir hmmbuild_out
-/afs/crc.nd.edu/user/m/mzarodn2/Private/Biocomputing/tools/hmmbuild ./hmmer_out/mcrAgene.hmm ./ref_sequences/mcrAgene_aligned.fasta
-/afs/crc.nd.edu/user/m/mzarodn2/Private/Biocomputing/tools/hmmbuild ./hmmer_out/hsp70gene.hmm ./ref_sequences/hsp70gene_aligned.fasta
+/afs/crc.nd.edu/user/m/mzarodn2/Private/Biocomputing/tools/hmmbuild ./hmmer_out/mcrAgene.hmm ./${REFSEQ_DIRNAME}/mcrAgene_aligned.fasta
+/afs/crc.nd.edu/user/m/mzarodn2/Private/Biocomputing/tools/hmmbuild ./hmmer_out/hsp70gene.hmm ./${REFSEQ_DIRNAME}/hsp70gene_aligned.fasta
 
 # Hmmsearch
 mkdir hmmsearch_out
-for i in "./proteomes/proteome_"*".fasta"
-do
-	proteome_no=$(echo $i | cut -f3 -d/ | cut -f1 -d. | cut -f2 -d_)
-	/afs/crc.nd.edu/user/m/mzarodn2/Private/Biocomputing/tools/hmmsearch --tblout "./hmmsearch_out/mcrAgene_hmmsearch_${proteome_no}.txt" ./hmmbuild_out/mcrAgene.hmm "$i"
-done
-
-
-for i in "./proteomes/proteome_"*".fasta"
-do
-	proteome_no=$(echo $i | cut -f3 -d/ | cut -f1 -d. | cut -f2 -d_)
-	/afs/crc.nd.edu/user/m/mzarodn2/Private/Biocomputing/tools/hmmsearch --tblout "./hmmsearch_out/hsp70gene_hmmsearch_${proteome_no}.txt" ./hmmbuild_out/hsp70gene.hmm "$i"
-done
-
-
-for i in "./proteomes/proteome_"*".fasta"
+for i in "./${PROTEOME_DIRNAME}/proteome_"*".fasta"
 do
 	proteome_no=$(echo $i | cut -f3 -d/ | cut -f1 -d. | cut -f2 -d_)
 	prefix=$(echo $i | cut -f3 -d/ | cut -f1 -d.)
+
+	/afs/crc.nd.edu/user/m/mzarodn2/Private/Biocomputing/tools/hmmsearch --tblout "./hmmsearch_out/mcrAgene_hmmsearch_${proteome_no}.txt" ./hmmbuild_out/mcrAgene.hmm "$i"
+	/afs/crc.nd.edu/user/m/mzarodn2/Private/Biocomputing/tools/hmmsearch --tblout "./hmmsearch_out/hsp70gene_hmmsearch_${proteome_no}.txt" ./hmmbuild_out/hsp70gene.hmm "$i"
+
 	hsp70_count=$(grep -vc "^#" "./hmmsearch_out/hsp70gene_hmmsearch_${proteome_no}.txt")
 	mcrA_count=$(grep -vc "^#" "./hmmsearch_out/mcrAgene_hmmsearch_${proteome_no}.txt")
-	echo -e $prefix"\t"$mcrA_count"\t"$hsp70_count >> hits_table.txt
+	echo $prefix","$mcrA_count","$hsp70_count >> hits_table.txt
 done
 
+rm candidate_methanogens.txt
 grep -wv 0 hits_table.txt | sort -rnk3 > candidate_methanogens.txt
