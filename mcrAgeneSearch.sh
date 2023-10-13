ab74b8b31b5f5c927d4fe526139f598b4af90121
Yovany Saravia <csaravi2@crcfe02.crc.nd.edu>
2023-10-13 15:29:53 -0400

    These are the files that were used to get the results

---
+++ mcrAgeneSearch.sh
@@ -0,0 +1,15 @@
+# usage: 
+
+#mkdir mcrAgeneResults
+
+for i in {1..9}
+do
+	./hmmsearch --tblout mcrAgeneProteome_$i.output mcrAgene.hmm ./proteomes/proteome_0$i.fasta
+	mv mcrAgeneProteome_$i.ouput mcrAgeneResults/ 
+done
+
+for i in {10..50}
+do
+	 ./hmmsearch --tblout mcrAgeneProteome_$i.output mcrAgene.hmm ./proteomes/proteome_$i.fasta
+	 mv mcrAgeneProteome_$i.ouput mcrAgeneResults/ 
+done
